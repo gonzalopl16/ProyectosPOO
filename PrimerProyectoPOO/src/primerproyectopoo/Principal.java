32b3dfd15e63965642e01d297831af2694c50729
Luis Gonzalo <renovado16@gmail.com>
2021-12-22 00:44:47 -0500

    Main

--- PrimerProyectoPOO/src/primerproyectopoo/Principal.java
+++ PrimerProyectoPOO/src/primerproyectopoo/Principal.java
@@ -14,7 +14,11 @@ public class Principal {
         CuentaBancaria obj2 = new CuentaBancaria();
         CuentaBancaria obj3 = new CuentaBancaria();
         
+        obj1.setAlias("miAlias");
+        obj1.setMoneda("Pesos");
+        obj1.setTipo("caja de ahorros");
         
+        obj2.setAlias("");
     }
     
 }
